8668b6b52633828906454108aa96519279043cd7
vkhurana2525 <126253345+vkhurana2525@users.noreply.github.com>
2023-02-28 09:22:08 +0530

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Question2{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        String inputStream="";
+        String input="";
         while(true){
             System.out.print("Enter text: ");
             String line=sc.nextLine();
             if(line.equals("XDONE")){
                 break;
             }
-            inputStream+=line+"\n";
+            input+=line+"\n";
         }
-        System.out.println("Entered text:");
-        System.out.println(inputStream);
+        System.out.println("Entered text is:");
+        System.out.println(input);
 
     }
 }
